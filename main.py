590c16909e7b3ba668eb4906f482169965d31c17
Mateusz Mocko <mati.mocko10@gmail.com>
2023-04-03 13:01:43 +0200

    Added NYtimes

--- main.py
+++ main.py
@@ -57,10 +57,24 @@ def search_reuters(query):
         # Open the search results in the default browser
         webbrowser.open_new_tab(url)
 
-        # Open the first article in a new tab
-        article_url = "https://www.reuters.com" + article_links[0]['href']
-        print(f"Opening URL: {article_url}")
-        webbrowser.open_new_tab(article_url)
+        # Print information for each article found
+        for i, link in enumerate(article_links):
+            article_title = link.text
+            article_description = link.find_previous('p')
+            if article_description:
+                article_description = article_description.text
+            else:
+                article_description = "No description available."
+            article_url = "https://www.reuters.com" + link['href']
+            print(f"\nArticle {i+1}:")
+            print(f"Title: {article_title}")
+            print(f"Description: {article_description}")
+            print(f"URL: {article_url}")
+
+            # Open the first article in a new tab
+            if i == 0:
+                print(f"\nOpening URL: {article_url}")
+                webbrowser.open_new_tab(article_url)
     else:
         print(f"No articles found related to '{query}'.")
 
@@ -94,10 +108,24 @@ def search_bbc(query):
         # Open the search results in the default browser
         webbrowser.open_new_tab(url)
 
-        # Open the first article in a new tab
-        article_url = article_links[0]['href']
-        print(f"Opening URL: {article_url}")
-        webbrowser.open_new_tab(article_url)
+        # Print information for each article found
+        for i, link in enumerate(article_links):
+            article_title = link.text
+            article_description = link.find_next('p')
+            if article_description:
+                article_description = article_description.text
+            else:
+                article_description = "No description available."
+            article_url = link['href']
+            print(f"\nArticle {i+1}:")
+            print(f"Title: {article_title}")
+            print(f"Description: {article_description}")
+            print(f"URL: {article_url}")
+
+            # Open the first article in a new tab
+            if i == 0:
+                print(f"\nOpening URL: {article_url}")
+                webbrowser.open_new_tab(article_url)
     else:
         print(f"No articles found related to '{query}'.")
 
@@ -124,6 +152,53 @@ def search_google(query):
         webbrowser.open_new_tab(search_result_url)
     else:
         print(f"No search results found related to '{query}'.")
+
+# function for searching nytimes
+
+
+def search_nytimes(query):
+    # Format the query for the NY Times search URL
+    query = query.replace(' ', '+')
+
+    # Send a GET request to the NY Times search URL
+    url = f'https://www.nytimes.com/search?query={query}'
+    response = requests.get(url)
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find all the article links in the search results
+    article_links = soup.select('div.css-e1lvw9 > a')
+
+    # Print the number of articles found
+    num_articles = len(article_links)
+    if num_articles:
+        print(f"Found {num_articles} articles related to '{query}':")
+        print("Opening the first article in a new tab and the search results in another...")
+
+        # Open the search results in the default browser
+        webbrowser.open_new_tab(url)
+
+        # Print information for each article found
+        for i, link in enumerate(article_links):
+            article_title = link.find('h4', {'class': 'css-2fgx4k'}).text
+            article_description = link.find('p', {'class': 'css-16nhkrn'})
+            if article_description:
+                article_description = article_description.text
+            else:
+                article_description = "No description available."
+            article_url = "https://www.nytimes.com/" + article_links[0]['href']
+            print(f"\nArticle {i+1}:")
+            print(f"Title: {article_title}")
+            print(f"Description: {article_description}")
+            print(f"URL: {article_url}")
+
+            # Open the first article in a new tab
+            if i == 0:
+                print(f"\nOpening URL: {article_url}")
+                webbrowser.open_new_tab(article_url)
+    else:
+        print(f"No articles found related to '{query}'.")
 
 
 while True:
@@ -174,9 +249,9 @@ while True:
     else:
         print("The phrase is possibly real. This answer is based on the probability of the prediction.")
 
-    # Ask the user if they want to search for the phrase in Reuters or BBC
+    # Ask the user if they want to search for the phrase in Reuters or BBC or Google or NYTimes or Quit
     search_input = input(
-        "Do you want to search for this phrase in Reuters / BBC? (R for Reuters/B for BBC/G for GOOGLE/Q for QUITTING THE PROGRAM): ")
+        "Do you want to search for this phrase in Reuters / BBC / Google / NYTimes / or Quit? (R for Reuters/B for BBC/G for GOOGLE/N for NYTimes/Q for QUITTING THE PROGRAM): ")
     print(search_input)
     if search_input.lower() == 'r':
         search_reuters(user_input)
@@ -184,6 +259,8 @@ while True:
         search_bbc(user_input)
     elif search_input.lower() == 'g':
         search_google(user_input)
+    elif search_input.lower() == 'n':
+        search_nytimes(user_input)
     elif search_input.lower() == 'q':
         break
     else:
